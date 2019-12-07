4d8fa81b800be8a862f39f5078a67d842a7236bb
Kenkhoo99 <k.layken99@gmail.com>
2019-12-07 23:29:44 +0800

    Assignment commit

---
+++ app/src/main/java/com/example/loginpage/Forgot_Password.kt
@@ -0,0 +1,12 @@
+package com.example.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Forgot_Password : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot__password)
+    }
+}
