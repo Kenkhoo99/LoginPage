f54aef41cfd1ea0760d2e46376180f39c6339619
Kenkhoo99 <k.layken99@gmail.com>
2019-12-10 14:06:30 +0800

    commit

--- app/src/main/java/com/example/loginpage/Login1.kt
+++ app/src/main/java/com/example/loginpage/Login1.kt
@@ -1,9 +1,11 @@
 package com.example.loginpage
 
+import android.content.Intent
 import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_login1.*
 
 class Login1 : AppCompatActivity() {
 
@@ -16,5 +18,11 @@ class Login1 : AppCompatActivity() {
 
         val button1 = findViewById<Button>(R.id.register2_button)
         button1.paintFlags = button1.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+        val button2 = findViewById<Button>(R.id.forgot_password)
+        button2.setOnClickListener{
+            val intent = Intent(this, Forgot_Password:: class.java)
+            startActivity(intent)
+        }
     }
 }
